fcabed2bd394890a687ab3ad9b8b370382a13514
NebulousCalm <88895551+NebulousCalm@users.noreply.github.com>
2024-01-05 13:47:29 +0000

    updating freshwater

--- app/app/freshwater/page.js
+++ app/app/freshwater/page.js
@@ -16,7 +16,7 @@ export default function astronomy() {
             </div>
 
             <div className="article-container">
-                <p className="paragraph">Groundwater (as depicted above) is heavily important</p>
+                <p className="paragraph">Freshwater (as depicted above) is heavily important to well, staying alive. It can originate from multiple sources. A river, lake, glaciers and groundwater to name a few. Water seeps into the ground, contributing to the water table. Which is the boundary between water-saturated ground and the unsaturated ground. We use wells (as seen far right) to go past the water table and take up water. However a less/non-permeable set of soil can sometimes interrupt the flow of water. This is called an aquitard (depicted on the left side).</p>
                 <p className="paragraph"></p>
                 <p className="paragraph"></p>
             </div>
