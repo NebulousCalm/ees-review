79d4ac279f75ccc1e51eee35d5892523f7cf8ac3
NebulousCalm <88895551+NebulousCalm@users.noreply.github.com>
2024-01-10 17:20:04 +0000

    ik where george lives

--- app/app/astronomy/page.js
+++ app/app/astronomy/page.js
@@ -1,10 +1,16 @@
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import Head from 'next/head'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFaceRelieved } from '@fortawesome/pro-solid-svg-icons'
 
-export default function astronomy() {
-    return (
-    <main>
-      <FontAwesomeIcon icon={faCoffee} />
-    </main>    
-    )
+export default function Home() {
+  return (
+    <div className="container">
+      <main>
+        <h1 className="title">
+          <FontAwesomeIcon icon={faFaceRelieved} />
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+      </main>
+    </div>
+  )
 }
