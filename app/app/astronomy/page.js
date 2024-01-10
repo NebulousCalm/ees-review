28c0c7884c94ec4a1d5a132589f02e8290494ce4
NebulousCalm <88895551+NebulousCalm@users.noreply.github.com>
2024-01-10 17:59:01 +0000

    spelling err?

--- app/app/astronomy/page.js
+++ app/app/astronomy/page.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFaceRelieved } from '@fortawesome/pro-solid-svg-icons'
+import { FontAwesomeIcon } from '@fontawesome/react-fontawesome'
+import { faFaceRelieved } from '@fontawesome/pro-solid-svg-icons'
 
 export default function Home() {
   return (
