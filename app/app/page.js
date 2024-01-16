52426b236beea5fa04197e908a55cc7e2a698405
NebulousCalm <88895551+NebulousCalm@users.noreply.github.com>
2024-01-16 16:25:12 +0000

    Added Github Link // PRODUCTION BUILD

--- app/app/page.js
+++ app/app/page.js
@@ -22,6 +22,7 @@ export default function Home() {
 
         <p id="explanation">This website, created as a High school Earth and Environmental final project to summarize all content during the semester. January, 2023</p>
         <Link href="https://ees-yearinreview.vercel.app/astronomy" className="getstarted-btn">Begin <i className="fa-solid fa-arrow-right right-arrow"></i></Link>
+        <Link href="https://github.com/NebulousCalm/ees-review/tree/main" className="getstarted-btn">Github <i className="fa-solid fa-arrow-right right-arrow"></i></Link>
       </div>
   )
 }
