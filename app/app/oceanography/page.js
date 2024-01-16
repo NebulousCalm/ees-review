674b01c3dff29184f12d1740b119add8535f7c06
NebulousCalm <88895551+NebulousCalm@users.noreply.github.com>
2024-01-16 12:53:46 +0000

    Oceanography done

--- app/app/oceanography/page.js
+++ app/app/oceanography/page.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMeteor, faLayerGroup, faWater, faCloudShowersWater, faVolcano, faIcicles, faRecycle, faPlug, faTree} from '@fortawesome/free-solid-svg-icons'
-export default function dynamearth() {
+export default function oceanography() {
     return (
         <main>
             <div className="sidenav">
@@ -21,15 +21,15 @@ export default function dynamearth() {
             <Link href="https://images.unsplash.com/photo-1553570739-330b8db8a925?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="image-container ocean"></Link>
             <div className="whyunderstand">
                 <h1 className="title">Facts from the unit</h1>
-                <p className="text"></p>
+                <p className="text">The Earth is covered in 71% water, of which originally came from comets and metiorites containing the valued h20. Volcanoes also contributed to water, as their volcanic plumes contain water vapor. Once the Earth began to cool, this vapor condensed into water. Forming the oceans we know today. There is a difference between an ocean and a sea. A sea is smaller than an ocean, as well as it must be partially or completely landlocked.</p>
             </div>
             <br />
             <div className="img-explain">
-                <p className="text"></p>
+                <p className="text">Above is an example of a Sea Arch. Formed by erosion, an opening is made. This is done by an enlargement of a sea cave (of which is formed primarily from sea waves). This leaves a bridge like gap over the water, in an arch shape. Hence the name.</p>
             </div>
             <div className="whythismatters">
                 <h1 className="title">How it affects me</h1>
-                <p className="text"></p>
+                <p className="text">As I've previously mentioned, the Earth is covered in 71% oceans. Meaning, no matter what I will come in contact with them. Sooner or later, whether it be rising sea levels from the melting ice caps or by going to a coastline. The ocean is a destructive and beautiful place that I want to understand more about in greater detail, and hopefully with my data collection I can do just that. </p>
             </div>
         </main>
     )
